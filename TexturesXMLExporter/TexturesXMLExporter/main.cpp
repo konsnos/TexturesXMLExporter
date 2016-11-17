747f01721e6d4a1f400d256f2881842802d0f145
Konstantinos Egkarchos <konsnosl@gmail.com>
2016-11-17 21:44:53 +0200

    removed usage of a local installation of image magick.

--- TexturesXMLExporter/TexturesXMLExporter/main.cpp
+++ TexturesXMLExporter/TexturesXMLExporter/main.cpp
@@ -20,7 +20,7 @@ void handleCMDArguments(int argc, wchar_t* argv[])
 
 		if (arg == "--help")			// Help
 		{
-			cout << "\nTexturesXMLExporter\n\nGenerate an xml containing all textures in material format inside their folders.\n\nOptional Arguments\n\n-p\n\tA custom path must be supplied\n\tExample: \"TexturesXMLExporter.exe -p C:\\Users\\Me\\Textures\".\n\tThe thumbs folder and the textures.xml file will be generated to this folder.\n\n-im\n\tUses a local version of ImageMagick. Specifically requires the full path of the executable convert.exe.\n\tExample: TexturesXMLExporter.exe -im C:\\Users\\Me\\Downloads\\ImageMagick - 6.9.1 - 9 - portable - Q16 - x64\convert.exe\n\n--version\n\tOutput version information and exit.\n\n--help\n\tDisplay this help and exit." << endl;
+			cout << "\nTexturesXMLExporter\n\nGenerate an xml containing all textures in material format inside their folders.\n\nOptional Arguments\n\n-p\n\tA custom path must be supplied\n\tExample: \"TexturesXMLExporter.exe -p C:\\Users\\Me\\Textures\".\n\tThe thumbs folder and the textures.xml file will be generated to this folder.\n\n--version\n\tOutput version information and exit.\n\n--help\n\tDisplay this help and exit." << endl;
 			exit(0);
 		}
 		else if (arg == "--version")	// Version
@@ -37,22 +37,6 @@ void handleCMDArguments(int argc, wchar_t* argv[])
 			if (i+1 != argc)
 			{
 				statics::startingPath = argv[++i];
-			}
-			else
-			{
-				cout << "Not enough parameters. Check --help ." << endl;
-				exit(1);
-			}
-		}
-		else if (arg == "-im")
-		{
-			/*
-			Example: "TexturesXMLExporter.exe -im C:\Users\Me\Downloads\ImageMagick-6.9.1-9-portable-Q16-x64\convert.exe"
-			*/
-			if (i + 1 != argc)
-			{
-				statics::convertLocal = true;
-				statics::convertPath = argv[++i];
 			}
 			else
 			{
